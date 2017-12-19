21561dc6f773949ecbccf6e947aa196256f6be78
伊勢　遼太郎 <ryotaro@iseryoutarou-no-MacBook.local>
2017-12-19 17:20:14 +0900

    テストケースの変更

--- s4/b173305/TestCase.java
+++ s4/b173305/TestCase.java
@@ -3,28 +3,28 @@ import java.lang.*;
 import s4.specification.*;
 
 /*
-interface FrequencerInterface {     // This interface provides the design for frequency counter.
-    void setTarget(byte[]  target); // set the data to search.
-    void setSpace(byte[]  space);  // set the data to be searched target from.
-    int frequency(); //It return -1, when TARGET is not set or TARGET's length is zero
-                    //Otherwise, it return 0, when SPACE is not set or Space's length is zero
-                    //Otherwise, get the frequency of TAGET in SPACE
+interface FrequencerInterface {     // このインタフェースは、周波数カウンタの設計を提供します。
+    void setTarget(byte[]  target); // 検索するデータを設定します。
+    void setSpace(byte[]  space);  // 検索対象となるデータを設定してください。
+    int frequency(); //TARGETが設定されていないか、TARGETの長さがゼロの場合は-1を返します。
+                    //それ以外の場合は、SPACEが設定されていないか、スペースの長さがゼロの場合は0を返します。
+                    //さもなければ、スペースのTARGETの頻度を得なさい
     int subByteFrequency(int start, int end);
-    // get the frequency of subByte of taget, i.e target[start], taget[start+1], ... , target[end-1].
-    // For the incorrect value of START or END, the behavior is undefined.
+    // target [start]、target [start + 1]、...、target [end-1]のターゲットのサブバイトの頻度を取得します。
+    // STARTまたはENDの値が正しくない場合の動作は未定義です。
 }
 */
 
 /*
 package s4.specification;
 public interface InformationEstimatorInterface{
-    void setTarget(byte target[]); // set the data for computing the information quantities
-    void setSpace(byte space[]); // set data for sample space to computer probability
-    double estimation(); // It returns 0.0 when the target is not set or Target's length is zero;
-// It returns Double.MAX_VALUE, when the true value is infinite, or space is not set.
-// The behavior is undefined, if the true value is finete but larger than Double.MAX_VALUE.
-// Note that this happens only when the space is unreasonably large. We will encounter other problem anyway.
-// Otherwise, estimation of information quantity,
+    void setTarget(byte target[]); // 情報量を算出するためのデータを設定する
+    void setSpace(byte space[]); // サンプル空間のデータをコンピュータ確率に設定する
+    double estimation(); // ターゲットが設定されていないか、ターゲットの長さがゼロの場合は0.0を返します。
+// true値が無限大の場合、または空白が設定されていない場合はDouble.MAX_VALUEを返します。
+// true値がfineteでDouble.MAX_VALUEより大きい場合の動作は未定義です。
+//これは、スペースが不当に大きい場合にのみ発生することに注意してください。 とにかく他の問題に遭遇します。
+// そうでなければ、情報量の推定、
 }
 */
 
@@ -36,14 +36,17 @@ public class TestCase {
 	    int freq;
 	    System.out.println("checking s4.b173305.Frequencer");
 	    myObject = new s4.b173305.Frequencer();
-	    myObject.setSpace("Hi Ho Hi Ho".getBytes());
-	    myObject.setTarget("H".getBytes());
+	   // myObject.setSpace("Hi Ho Hi Ho".getBytes());
+      myObject.setSpace("Hi Ho Hi Ho".getBytes());
+	   // myObject.setTarget("H".getBytes());
 
       //テストとして文字列が"0"の場合を追加する
       myObject.setTarget("".getBytes());
 
 	    freq = myObject.frequency();
-	    System.out.print("\"H\" in \"Hi Ho Hi Ho\" appears "+freq+" times. ");
+      System.out.println(freq);
+
+	    System.out.print("\"\" in \"Hi Ho Hi Ho\" appears "+freq+" times. ");
 	    if(4 == freq) { System.out.println("OK"); } else {System.out.println("WRONG"); }
 	}
 	catch(Exception e) {
